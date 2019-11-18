032256ee6b59db22ccc47a4e3ebedd5ed9d1fc00
stinta <stinta@gmail.com>
2019-11-18 04:57:29 -0500

    added logic to publish velocity command with conservative motion

--- src/walker_gazebo.cpp
+++ src/walker_gazebo.cpp
@@ -38,6 +38,7 @@
 
 #include "ros/ros.h"
 #include "sensor_msgs/LaserScan.h"
+#include "geometry_msgs/Twist.h"
 #include "walker_gazebo/laserReadHelper.h"
 
 int main(int argc, char **argv) {
@@ -82,13 +83,60 @@ int main(int argc, char **argv) {
    */
   auto sub = n.subscribe<sensor_msgs::LaserScan>("/scan", 1000, &LaserReadHelper::processLaserScan,&lrhelper);
 
-  /**
-   * ros::spin() will enter a loop, pumping callbacks.  With this version, all
-   * callbacks will be called from within this thread (the main one).  ros::spin()
-   * will exit when Ctrl-C is pressed, or the node is shutdown by the master.
+/**
+   * The advertise() function is how you tell ROS that you want to
+   * publish on a given topic name. This invokes a call to the ROS
+   * master node, which keeps a registry of who is publishing and who
+   * is subscribing. After this advertise() call is made, the master
+   * node will notify anyone who is trying to subscribe to this topic name,
+   * and they will in turn negotiate a peer-to-peer connection with this
+   * node.  advertise() returns a Publisher object which allows you to
+   * publish messages on that topic through a call to publish().  Once
+   * all copies of the returned Publisher object are destroyed, the topic
+   * will be automatically unadvertised.
+   *
+   * The second parameter to advertise() is the size of the message queue
+   * used for publishing messages.  If messages are published more quickly
+   * than we can send them, the number here specifies how many messages to
+   * buffer up before throwing some away.
    */
-  ros::spin();
-  ROS_WARN_STREAM("After ros::spin; program is ending");
+  auto vel_pub = n.advertise<geometry_msgs::Twist>("/mobile_base/commands/velocity", 1000);
+  double lcl_rate = 10;
+  geometry_msgs::Twist vel_msg;
+  ros::Rate loop_rate(lcl_rate);
+
+while (ros::ok()) {
+    /**
+     * if the wall was detected rotate robot
+     */
+    if (lrhelper.getWallInFront()){
+        // stop moving forward
+	vel_msg.linear.x = 0.0;
+        // rotate laser
+        vel_msg.angular.z = 0.25;
+    }
+   else { // no obstacle detected
+       //  move forward
+	vel_msg.linear.x = 0.3;
+        // go straight
+        vel_msg.angular.z = 0;
+    }
+    
+    //ROS_INFO("%s", msg.data.c_str());
+
+    /**
+     * The publish() function is how you send messages. The parameter
+     * is the message object. The type of this object must agree with the type
+     * given as a template parameter to the advertise<>() call, as was done
+     * in the constructor above.
+     */
+     vel_pub.publish(vel_msg);
+
+
+    ros::spinOnce();
+
+    loop_rate.sleep();
+  }
 
   return 0;
 }
