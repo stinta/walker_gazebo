1ba1bd1cbf7b834c3c28216792778da084a2eba8
stinta <stinta@gmail.com>
2019-11-18 04:32:01 -0500

    added initial logic to flag detection of the wall

--- src/LaserReadHelper.cpp
+++ src/LaserReadHelper.cpp
@@ -47,7 +47,9 @@
    /*
     * @brief constructor
     */
-    LaserReadHelper::LaserReadHelper(){};
+    LaserReadHelper::LaserReadHelper(){
+      wallInFront = false;
+    };
     /*
      * @brief virtual destructor
      */
@@ -60,58 +62,22 @@
      *
      * @param message published by LaserScan
      */
-    void LaserReadHelper::processLaserScan(const sensor_msgs::LaserScan::ConstPtr&) {};
+    void LaserReadHelper::processLaserScan(const sensor_msgs::LaserScan::ConstPtr& scan) {
+	ROS_INFO_STREAM("Extracting range distance ahead of range sensor "<< scan->ranges[320]);
+	if (scan->ranges[320] > 1.0) {
+	   wallInFront =  false;
+        } 
+        else {
+           wallInFront = true;
+           ROS_INFO_STREAM("Wall in front detected");
+        }
+     } 
+    /*
+     * @brief Implements the callback funiton for LaserScan Subscriber
+     *
+     */
+    bool LaserReadHelper::getWallInFront(){
+      return wallInFront;
+    }
+	
 
-
-
-/*void chatterCallback(const std_msgs::String::ConstPtr& msg) {
-  ROS_INFO_STREAM("I heard: [" <<  msg->data.c_str() <<"]");
-}*/
-
-//int main(int argc, char **argv) {
-  /**
-   * The ros::init() function needs to see argc and argv so that it can perform
-   * any ROS arguments and name remapping that were provided at the command line.
-   * For programmatic remappings you can use a different version of init() which takes
-   * remappings directly, but for most command-line programs, passing argc and argv is
-   * the easiest way to do it.  The third argument to init() is the name of the node.
-   *
-   * You must call one of the versions of ros::init() before using any other
-   * part of the ROS system.
-   */
-  //ros::init(argc, argv, "walker_gazebo");
-
-  /**
-   * NodeHandle is the main access point to communications with the ROS system.
-   * The first NodeHandle constructed will fully initialize this node, and the last
-   * NodeHandle destructed will close down the node.
-   */
-  //ros::NodeHandle n = ros::NodeHandle();
-
-  /**
-   * The subscribe() call is how you tell ROS that you want to receive messages
-   * on a given topic.  This invokes a call to the ROS
-   * master node, which keeps a registry of who is publishing and who
-   * is subscribing.  Messages are passed to a callback function, here
-   * called chatterCallback.  subscribe() returns a Subscriber object that you
-   * must hold on to until you want to unsubscribe.  When all copies of the Subscriber
-   * object go out of scope, this callback will automatically be unsubscribed from
-   * this topic.
-   *
-   * The second parameter to the subscribe() function is the size of the message
-   * queue.  If messages are arriving faster than they are being processed, this
-   * is the number of messages that will be buffered up before beginning to throw
-   * away the oldest ones.
-   */
-  //auto sub = n.subscribe("chatter", 1000, chatterCallback);
-
-  /**
-   * ros::spin() will enter a loop, pumping callbacks.  With this version, all
-   * callbacks will be called from within this thread (the main one).  ros::spin()
-   * will exit when Ctrl-C is pressed, or the node is shutdown by the master.
-   */
-  //ros::spin();
-  //ROS_WARN_STREAM("After ros::spin; program is ending");
-
-  //return 0;
-//}
